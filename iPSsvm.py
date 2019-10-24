ec619bb1d9e3f6614e8d9c8b0eb2ee3c9fe2e673
kleinholdermann <urs@kleinholdermann.de>
2019-10-24 23:55:20 +0200

    Feature extraction implemented

--- iPSsvm.py
+++ iPSsvm.py
@@ -7,6 +7,7 @@ import numpy as np
 import pandas as pds
 from tsfresh import extract_features
 
+#if False:
 if True:
     # loads and preprocesses data as needed
     datobj = preprocess.dataworks.DataPipeline('', 'all', 'all', 'all',ignbad=True)
@@ -17,8 +18,21 @@ if True:
     trainX, trainy = catobj.create_cat(smplsONtrain, smplsOFFtrain)
     testX, testy = catobj.create_cat(smplsONtest, smplsOFFtest)
 
-a = trainX[0,:,:]
-time = np.arange(a.shape[0])
-b = np.append(np.reshape(time,(a.shape[0],1)),a,axis = 1)
-c = np.append(np.ones((a.shape[0],1)),b,axis = 1)
-df = pds.DataFrame(data = c[:,2:], index = c[:,1])
+time = np.arange(trainX[0,:,:].shape[0])
+firstrun = True
+for i in range(0, trainX.shape[0]):
+    curr_data = trainX[i,:,:]
+    curr_data = np.append(np.reshape(time, (curr_data.shape[0], 1)), curr_data, axis = 1)
+    curr_data = np.append(np.ones((curr_data.shape[0],1)), curr_data, axis = 1)
+    df = pds.DataFrame(curr_data)
+    ef = extract_features(df, column_id = 0, column_sort = 1)
+    if firstrun:
+        X = ef
+        firstrun = False
+    else:
+        X = np.append(X, ef, axis = 0)
+
+
+# run script from console like this:
+# exec(open('iPSsvm.py').read())
+
