3319749b65bd92d70cf2a4c64301b9bb0f5820b1
Kleinholdermann <urs@kleinholdermann.de>
2020-05-22 01:59:04 +0200

    updated emg article, updated plot file for emg article

--- R/plot_article_shallow_emg_grid_search_learning.R
+++ R/plot_article_shallow_emg_grid_search_learning.R
@@ -6,27 +6,26 @@ stat = read.csv(fname)
 # define plotfunction
 plotfun <- function(varname)
 {
-  p = ggplot(data=stat, 
-       aes(x=seconds, y=dv, 
-           group=datalength,
-           shape=datalength,
-           color=datalength)) + 
-               geom_line() + 
-               geom_point() +
-               scale_x_discrete("seconds") +
-               scale_y_continuous(varname) + 
-             facet_grid(.~method*featureset )     
+  p = ggplot( data=stat
+            , aes(x=seconds, y=dv))    + 
+      geom_line(color = "blue")                      + 
+      geom_point(color = "blue", size = 3)                     +
+      geom_errorbar(aes(ymin = corll
+                       ,ymax = corul
+		       ,width = 0)) +
+      scale_x_continuous("seconds", breaks = c(1:7))      +
+      scale_y_continuous(varname)      + 
+      facet_grid(.~method*featureset )     
 return(p)
 }
 
 stat = data.frame(stat)
+stat = subset(stat, datalength == "same")
 stat$dv = stat$cor
 p1 = plotfun("correlation")
 p1 + theme(panel.spacing.x=unit(c(1,1,1,5), "lines"))    
-stat$dv = stat$rmse
-p2 = plotfun("rmse")
 
-jpeg(file=paste(fname,"a.jpeg",sep=""),1600,900)
+#jpeg(file=paste(fname,"_paper.jpeg",sep=""),1600,900)
 print(p1)
-dev.off()
+#dev.off()
 
